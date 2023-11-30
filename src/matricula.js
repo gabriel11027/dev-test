0c66c2044554bf887ccbd7a057f65da2a05989e0
gabriel <gabrielpitondo@alunos.utfpr.edu.br>
2023-11-30 13:51:23 -0300

    Segunda e terceira etapas do formulário concluidas

--- src/matricula.js
+++ src/matricula.js
@@ -7,6 +7,13 @@ $(document).ready(function() {
         curso = "option[value='" + cursoSelecionado + "']";
         $("option[value=" + cursoSelecionado + "]").attr("selected", true);
     }
+
+    $("#inputTelefone").mask("(99) 99999-9999");
+    $("#inputCEP").mask("99999-999");
+    $("#inputCPF").mask("999.999.999-99");
+    $("#inputNascimento").mask("99/99/9999");
+    mostrarProximaEtapa("terceira-etapa");
+
 });
 
 
@@ -22,6 +29,30 @@ function mostrarProximaEtapa(proximaEtapa)
     $("." + proximaEtapa).removeClass("d-none");
 }
 
+function mostrarErros(erros) 
+{
+    // Tabela para mapear os erros do servidor para os inputs do formulário
+    erro_formulario = {
+        "erro_nome"      : "inputNomeCompleto",
+        "erro_email"     : "inputEmail",
+        "erro_telefone"  : "inputTelefone",
+        "erro_curso"     : "inputCursoSelecionado",
+        "erro_nascimento": "inputNascimento",
+        "erro_cep"       : "inputCEP",
+        "erro_cpf"       : "inputCPF"
+    }
+
+    for (const [chave, valor] of Object.entries(erro_formulario)) {
+        if (erros.includes(chave)) {
+            $("#" + valor).addClass("is-invalid");
+            $("#" + valor).removeClass("is-valid");
+        } else {
+            $("#" + valor).removeClass("is-invalid");
+        }
+    }
+}
+
+
 $("button[name='btn-primeira-etapa'], button[name='btn-segunda-etapa']").click(function(e) {
     e.preventDefault();
     mostrarProximaEtapa($(this).attr("name").slice(4));
@@ -30,36 +61,70 @@ $("button[name='btn-primeira-etapa'], button[name='btn-segunda-etapa']").click(f
 
 $("button[name='submeter-primeira-etapa']").on("click", function(e) {
     e.preventDefault();
-    mostrarProximaEtapa("segunda-etapa");
 
-    data_input = {
+    dados_primeira_etapa = {
         "submeter-primeira-etapa": true,
-        "inputNomeCompleto": $("#inputNomeCompleto").val(),
-        "inputEmail": $("#inputEmail").val(),
-        "inputTelefone": $("#inputTelefone").val(),
-        "inputCursoSelecionado": $("#inputCursoSelecionado").val()
+        "inputNomeCompleto"      : $("#inputNomeCompleto").val(),
+        "inputEmail"             : $("#inputEmail").val(),
+        "inputTelefone"          : $("#inputTelefone").val(),
+        "inputCursoSelecionado"  : $("#inputCursoSelecionado").val()
     }
-
+    
     $.ajax({
-        type: "POST",
-        url: "matricula.php",
-        data: data_input,
+        url     : "php/matricula.php",
+        type    : "POST",
+        data    : dados_primeira_etapa,
         dataType: "json",
-        encode: true
-    }).done(function (data) {
-        // Mostrar erros
+        encode  : true
+    }).done(function (resposta_servidor) {
+        mostrarErros(resposta_servidor["erros"]);
+        if(resposta_servidor["sucesso"] == 1)
+            mostrarProximaEtapa("segunda-etapa");
     });
 }); 
 
 $("button[name='submeter-segunda-etapa']").on("click", function(e) {
     e.preventDefault();
-    mostrarProximaEtapa("terceira-etapa");
+
+    dados_segunda_etapa = {
+        "submeter-segunda-etapa": true,
+        "inputCEP"              : $("#inputCEP").val(),
+        "inputCPF"              : $("#inputCPF").val(),
+        "inputNascimento"       : $("#inputNascimento").val()
+    }
+    
+    $.ajax({
+        url     : "php/matricula.php",
+        type    : "POST",
+        data    : dados_segunda_etapa,
+        dataType: "json",
+        encode  : true
+    }).done(function (resposta_servidor) {
+        mostrarErros(resposta_servidor["erros"])
+        if(resposta_servidor["sucesso"] == 1)
+            mostrarProximaEtapa("terceira-etapa");
+    });
 });
 
 $("button[name='submeter-terceira-etapa']").on("click", function(e) {
+    e.preventDefault();
+
+    var dados_terceira_etapa = new FormData();
+    dados_terceira_etapa.append("submeter-terceira-etapa", true)
+    dados_terceira_etapa.append("inputDocumento", document.getElementById("inputDocumento").files[0]);
+    console.log(dados_terceira_etapa)
+
+    $.ajax({
+        url        : "php/matricula.php",
+        type       : "POST",
+        data       : dados_terceira_etapa,
+        processData: false, 
+        contentType: false,
+    }).done(function (resposta_servidor) {
+        console.log(resposta_servidor)
+
+        if(resposta_servidor["sucesso"] == 1)
+            mostrarProximaEtapa("terceira-etapa");
+    });
 
 });
-
-$(window).bind("beforeunload", function() {
-       // Salvar formulário antes de fechar a janela
-}); 
--- src/php/matricula.php
+++ src/php/matricula.php
@@ -8,7 +8,7 @@
     }
 
     // Primeira etapa
-    if (isset($_POST['submeter-primeira-etapa'])) {       
+    if (isset($_POST["submeter-primeira-etapa"])) {       
         $resposta = array();
         $erros = array();
         $sucesso = 1;
@@ -16,33 +16,33 @@
         $inputNomeCompleto = htmlspecialchars($_POST["inputNomeCompleto"]);
         $inputCursoSelecionado = ($_POST["inputCursoSelecionado"]); 
         $inputEmail = ($_POST["inputEmail"]); 
-        $inputTelefone = str_replace(array("(", ")", "-"), "", $_POST["inputTelefone"]); 
+        $inputTelefone = str_replace(array("(", ")", "-", " "), "", $_POST["inputTelefone"]); 
 
 
         // Validação da primeira etapa
         
         // Nome inválido
         if (strlen($inputNomeCompleto) > 255 || strlen($inputNomeCompleto) < 5) {
-            $sucesso = 0;
             array_push($erros, "erro_nome");
+            $sucesso = 0;
         }
 
         // Curso não selecionado
         if (empty($inputCursoSelecionado)) {
-            $sucesso = 0;
             array_push($erros, "erro_curso");
+            $sucesso = 0;
         }
 
         // Email inválido 
         if (!filter_var($inputEmail, FILTER_VALIDATE_EMAIL)) {
-            $sucesso = 0;
             array_push($erros, "erro_email");
+            $sucesso = 0;
         }
        
         // Telefone inválido (contém números) ou (tamanho != 9 e tamanho !=11)
-        if (!preg_match("/^\d+$/", $inputTelefone) || (strlen($inputTelefone) != 9 && strlen($inputTelefone) != 11)) {
-            $sucesso = 0;
+        if (!preg_match("/^\d{11}$/", $inputTelefone)) {
             array_push($erros, "erro_telefone");
+            $sucesso = 0;
         }
         
         if ($sucesso) {
@@ -61,8 +61,9 @@
             $houve_mudança = 1;
 
             // Checa se o usuário voltou para mudar algum dado, a fim de evitar entras repetidas na base de dados
-            if ($_SESSION["NOME"] == $inputNomeCompleto && $_SESSION["CURSO"] = $inputCursoSelecionado &&
-                $_SESSION["EMAIL"] == $inputEmail && $_SESSION["TELEFONE"] = $inputTelefone ) {
+            if ($_SESSION["NOME"] == $inputNomeCompleto && $_SESSION["CURSO"] = $inputCursoSelecionado && 
+                $_SESSION["EMAIL"] == $inputEmail && $_SESSION["TELEFONE"] = $inputTelefone ) 
+            {
                     $houve_mudança = 0;
             }
 
@@ -81,5 +82,76 @@
         $resposta["erros"] = $erros;
         $resposta["sucesso"] = $sucesso;
         echo json_encode($resposta);
+    }
+
+
+    // Segunda etapa
+    if (isset($_POST["submeter-segunda-etapa"])) {
+        $resposta = array();
+        $erros = array();
+        $sucesso = 1;
+    
+        $inputCEP = $_POST["inputCEP"];
+        $inputCPF = $_POST["inputCPF"]; 
+        $inputNascimento = $_POST["inputNascimento"];
+        list($dia, $mes, $ano) = explode("/", $inputNascimento);
+            
+        // Validar data de nascimento
+        if (!preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $inputNascimento) || !checkdate($mes, $dia, $ano)) {
+            array_push($erros, "erro_nascimento");
+            $sucesso = 0;
         }
+
+        // Validar CEP (somente 8 números)
+        if (!preg_match("/^\d{5}\-\d{3}$/", $inputCEP)) {
+            array_push($erros, "erro_cep");
+            $sucesso = 0;
+        }
+
+        // Validar CPF (somente 11 números)
+        if (!preg_match("/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/", $inputCPF)) {
+            array_push($erros, "erro_cpf");
+            $sucesso = 0;
+        }
+
+        if ($sucesso) {
+            $_SESSION["CEP"] = $inputCEP;
+            $_SESSION["CPF"] = $inputCPF;
+            $_SESSION["DATA_DE_NASCIMENTO"] = $inputNascimento;
+        }
+
+        $resposta["erros"] = $erros;
+        $resposta["sucesso"] = $sucesso;
+        array_push($resposta, $erros);
+        echo json_encode($resposta);
+    }
+
+    if (isset($_POST["submeter-terceira-etapa"])) {
+        $resposta = array();
+        $erros = array();
+        $sucesso = 1;
+
+        $documento = $_FILES["inputDocumento"]["tmp_name"];
+        $tamanho = filesize($documento);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $finfo_file = NULL;
+
+        // Valida se algum arquivo foi enviado
+        if (!isset($_FILES["inputDocumento"])) {
+            array_push($erros, "erro_documento");
+            $sucesso = 0;
+        } else {
+            $finfo_file = finfo_file($finfo, $documento);
+        }
+      
+        // Testa se o formato do arquivo está em pdf
+        if ($finfo_file != "application/pdf") {
+            array_push($erros, "erro_pdf");
+            $sucesso = 0;
+        }
+
+        $resposta["erros"] = $erros;
+        $resposta["sucesso"] = $sucesso;
+        echo json_encode($resposta);
+    }
 ?>
